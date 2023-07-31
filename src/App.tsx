2c587eb9dcabbb30997ada8a79d307ed6bef21f1
Maksym Bobryk <booobryyyk@gmail.com>
2023-07-31 21:34:15 +0300

    fixed app.tsx after merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -26,21 +26,7 @@ export const App = () => (
         <Route path=":selectedSlug" element={<Accessories/>} />
       </Route>
 
-function App() {
-  return (
-    <>
-      <html lang="en">
-        <head>
-          <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>Document</title>
-        </head>
-        <body>
-          <Header />
-        </body>
-      </html>
-    </>
-  );
-}
-
-export default App;
+      {/* <Route path="*" element={<NotFound />} /> */}
+    </Route>
+  </Routes>
+);
