beafad879d50783d7988ea01a6cf11118ea7e138
Bohdan <6alfabravo9@gmail.com>
2023-08-01 14:13:06 +0300

    split styles and add navigation logic

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,73 +1,43 @@
 import React from 'react';
+import { API_URL } from '../../consts/api';
 import { Product } from '../../types/Product';
 
 type ProductCardProps = {
-  product: Product,
+  product: Product;
 };
 
 export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
-  const {
-    image,
-    name,
-    capacity,
-    color,
-    fullPrice,
-    price,
-    screen,
-    ram
-  } = product;
+  const { image, name, capacity, color, fullPrice, price, screen, ram } =
+    product;
+
+  const imageURL = API_URL + image;
+
   return (
-    <article className='product-card'>
-      <img
-        src={image}
-        className='product-card__image'
-        alt={name}
-      >
-      </img>
-      <h2 className="product-card__name">
-        {`${name} ${capacity} ${color}`}
-      </h2>
+    <article className="product-card">
+      <img src={imageURL} className="product-card__image" alt={name}></img>
+      <h2 className="product-card__name">{`${name} ${capacity} ${color}`}</h2>
       <div className="product-card__prices">
-        <div className="product-card__price">
-          {`$${price}`}
-        </div>
-        <div className="product-card__fullprice">
-          {`$${fullPrice}`} 
-        </div>
+        <div className="product-card__price">{`$${price}`}</div>
+        <div className="product-card__fullprice">{`$${fullPrice}`}</div>
       </div>
       <ul className="product-card__attributes">
-        <li className='product-card__attribute'>
-          <p className="product-card__attribute-title">
-            Screen
-          </p>
-          <p className="product-card__attribute-value">
-            {screen}
-          </p>
+        <li className="product-card__attribute">
+          <p className="product-card__attribute-title">Screen</p>
+          <p className="product-card__attribute-value">{screen}</p>
         </li>
-        <li className='product-card__attribute'>
-          <p className="product-card__attribute-title">
-            Capacity
-          </p>
-          <p className="product-card__attribute-value">
-            {capacity}
-          </p>
+        <li className="product-card__attribute">
+          <p className="product-card__attribute-title">Capacity</p>
+          <p className="product-card__attribute-value">{capacity}</p>
         </li>
-        <li className='product-card__attribute'>
-          <p className="product-card__attribute-title">
-            RAM
-          </p>
-          <p className="product-card__attribute-value">
-            {ram}
-          </p>
+        <li className="product-card__attribute">
+          <p className="product-card__attribute-title">RAM</p>
+          <p className="product-card__attribute-value">{ram}</p>
         </li>
       </ul>
       <div className="product-card__buttons">
-        <button className='product-card__cart-button'>
-          Add to cart
-        </button>
+        <button className="product-card__cart-button">Add to cart</button>
 
-        <button className='product-card__favorite-button'>
-        </button>
+        <button className="product-card__favorite-button"></button>
       </div>
     </article>
   );
