f1fc35e3ee4edfa6b3439d68a259714ffed3604b
Yuliia Kucheriavenko <kucherisvenko@gmail.com>
2023-08-02 12:21:45 +0300

    add fixed footer

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -4,17 +4,17 @@ import { Outlet } from 'react-router-dom';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 // import { Navigation } from '../../components/Navigation';
+import styles from './MainLayout.module.scss';
 
 export const MainLayout: React.FC = () => (
-  <div data-cy="app">
+  <div data-cy="app" className={styles.app}>
     <Header />
 
-    <main className="section">
-      <div className="container">
+    <main className={styles.section}>
+      <div className={styles.container}>
         <Outlet />
       </div>
     </main>
-
     <Footer />
   </div>
 );
