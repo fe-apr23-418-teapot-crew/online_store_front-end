3d2bf7e2a59a56fc20c742e0ef45b36a89bfbc33
Yuliia Kucheriavenko <kucherisvenko@gmail.com>
2023-08-03 10:33:28 +0300

    feat(cartItem): modal window & ui  cart item

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -2,15 +2,68 @@ import React, { useState } from 'react';
 import { CartItem } from '../../components/CartItem/CartItem';
 import { MenuLink } from '../../components/MenuLink';
 import { Product } from '../../types/Product';
+import { ModalWindow } from '../../components/ModalWindow/ModalWindow';
 import styles from './Cart.module.scss';
+import chevron from '../../icons/Chevron (Arrow Right).svg';
+import lineCheckout from '../../icons/LineCheckout.svg';
 
 interface CartProps {}
+
+// const TEMP_DATA = [
+//   {
+//     id: 1,
+//     category: 'phone',
+//     itemId: '0',
+//     name: 'Apple iPhone 14 Pro 128GB Silver (MQ023)',
+//     fullPrice: 1000,
+//     price: 999,
+//     screen: '15',
+//     capacity: '12',
+//     color: 'black',
+//     ram: '12',
+//     year: 2020,
+//     image: 'https://content1.rozetka.com.ua/goods/images/big/284920757.jpg',
+//   },
+//   {
+//     id: 1,
+//     category: 'phone',
+//     itemId: '0',
+//     name: 'Apple iPhone 14 Plus 128GB PRODUCT Red (MQ513)',
+//     fullPrice: 1000,
+//     price: 1012,
+//     screen: '15',
+//     capacity: '12',
+//     color: 'black',
+//     ram: '12',
+//     year: 2020,
+//     image: 'https://content1.rozetka.com.ua/goods/images/big/284920757.jpg',
+//   },
+//   {
+//     id: 1,
+//     category: 'phone',
+//     itemId: '0',
+//     name: 'Apple iPhone 14 Pro Plus 128GB PRODUCT Red (MQ513)',
+//     fullPrice: 1000,
+//     price: 856,
+//     screen: '15',
+//     capacity: '12',
+//     color: 'black',
+//     ram: '12',
+//     year: 2020,
+//     image: 'https://content1.rozetka.com.ua/goods/images/big/284920757.jpg',
+//   }
+// ];
 
 export const Cart: React.FC<CartProps> = () => {
   const existingCartProducts = localStorage.getItem('cartItems');
   const [cartProducts, setCartProducts] = useState<Product[]>(
     existingCartProducts ? JSON.parse(existingCartProducts) : [],
   );
+
+  const [isModal, setIsModal] = useState(false);
+
+
+  console.log(cartProducts);
 
   const [totalAmount, setTotalAmount] = useState(0);
   const totalCartItemsCount = cartProducts.length;
@@ -26,10 +79,17 @@ export const Cart: React.FC<CartProps> = () => {
 
   return (
     <section className={styles.cart}>
-      <div className="container">
+      <div className={styles.container}>
         <div className={styles.cart__navigation}>
           <MenuLink to="/" path="Back">
-            {'< Back'}
+            <img 
+              src={chevron}
+              alt='Chevron'
+              className={styles.cart__navigation__chevronButton}
+            />
+            <div className={styles.cart__navigation__backBotton}>
+              {'Back'}
+            </div>
           </MenuLink>
         </div>
 
@@ -50,18 +110,28 @@ export const Cart: React.FC<CartProps> = () => {
 
             <div className={styles.cart__from}>
               <div className={styles.cart__total}>
-                <span className={styles.cart__totalAmout}>{totalAmount}</span>
+                <span className={styles.cart__totalAmout}>{`$${totalAmount}`}</span>
 
                 <span className={styles.cart__totalCount}>
-                  {`Total for ${totalCartItemsCount}`}
+                  {`Total for ${totalCartItemsCount} items`}
                 </span>
               </div>
-
-              <button className={styles.cart__button}>Checkout</button>
+              <img 
+                src={lineCheckout}
+                alt='lineCheckout'
+                className={styles.lineCheckout}
+              />
+              <button 
+                className={styles.cart__button}
+                onClick={() => setIsModal(true)}
+              >
+                Checkout
+              </button>
             </div>
           </div>
         )}
       </div>
+      {isModal && <ModalWindow />}
     </section>
   );
 };
