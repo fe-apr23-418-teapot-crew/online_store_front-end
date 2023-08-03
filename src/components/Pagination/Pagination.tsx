367bed8e5d84fc7cb0f7f981825d1223652167ab
Vania Yarovyi <yarovyi@gmail.com>
2023-08-03 22:10:24 +0300

    fixed pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,10 +1,7 @@
 import React, { useState, useMemo } from 'react';
 import cn from 'classnames';
 import styles from './Pagination.module.scss';
-//import { ProductCard } from '../ProductCard';
 import { Product } from '../../types/Product';
-
-// ! Used the phones file instead of server data
 
 interface Props {
   pages: number;
@@ -15,14 +12,6 @@ export const Pagination: React.FC<Props> = ({ pages, products }) => {
   const [activePage, setActivePage] = useState<number>(1);
   const [translateX, setTranslateX] = useState(0);
   const lastPage = Math.ceil(products.length / pages);
-  const displayedPhones = useMemo(() => {
-    return products.filter(
-      (phone) =>
-        +phone.id > pages * (activePage - 1) && +phone.id <= pages * activePage,
-    );
-  }, [products, pages, activePage]);
-
-  console.log(displayedPhones);
 
   const pageNumbers = useMemo(() => {
     const lastPage = Math.ceil(products.length / pages);
@@ -54,48 +43,40 @@ export const Pagination: React.FC<Props> = ({ pages, products }) => {
   };
 
   return (
-    <>
-      <ul>
-        {/* {displayedPhones.map((gadget) => (
-          //<ProductCard key={gadget.id} product={gadget} onChangeSelectedProductId/>
-        ))} */}
-        <li>1</li>
-      </ul>
-      <aside className={styles.pagination}>
-        <button
-          className={`${styles.pagination__button} ${styles['pagination__button--arrow']}`}
-          onClick={handleMoveLeft}
+    <aside className={styles.pagination}>
+      <button
+        className={`${styles.pagination__button} ${styles['pagination__button--arrow']}`}
+        onClick={handleMoveLeft}
+      >
+        {'<'}
+      </button>
+      <div className={styles['pagination__pages-list-container']}>
+        <div
+          className={styles['pagination__pages-list']}
+          style={{
+            transform: `translateX(${translateX}px)`,
+            transition: 'transform 0.3s ease',
+          }}
         >
-          {'<'}
-        </button>
-        <div className={styles['pagination__pages-list-container']}>
-          <div
-            className={styles['pagination__pages-list']}
-            style={{
-              transform: `translateX(${translateX}px)`,
-              transition: 'transform 0.3s ease',
-            }}
-          >
-            {pageNumbers.map((num) => (
-              <button
-                className={cn(styles.pagination__button, {
-                  [styles['pagination__button--active']]: activePage === num,
-                })}
-                key={num}
-                onClick={() => setActivePage(num)}
-              >
-                {num}
-              </button>
-            ))}
-          </div>
+          {pageNumbers.map((num) => (
+            <button
+              className={cn(styles.pagination__button, {
+                [styles['pagination__button--active']]: activePage === num,
+              })}
+              key={num}
+              onClick={() => setActivePage(num)}
+            >
+              {num}
+            </button>
+          ))}
         </div>
-        <button
-          className={`${styles.pagination__button} ${styles['pagination__button--arrow']}`}
-          onClick={handleMoveRight}
-        >
-          {'>'}
-        </button>
-      </aside>
-    </>
+      </div>
+      <button
+        className={`${styles.pagination__button} ${styles['pagination__button--arrow']}`}
+        onClick={handleMoveRight}
+      >
+        {'>'}
+      </button>
+    </aside>
   );
 };
