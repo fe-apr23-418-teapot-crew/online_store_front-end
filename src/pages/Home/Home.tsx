76d7e47e68df5985dbabc0c2345830640836cca7
Maksym Bobryk <booobryyyk@gmail.com>
2023-08-09 17:28:53 +0300

    fixed styles

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Categories } from '../../components/Categories';
 import styles from './Home.module.scss';
 import { CardCarousel } from '../../components/CardCarousel/CardCarousel';
+import { BannerSlider } from '../../components/BannerSlider/BannerSlider';
 
 export const Home: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ export const Home: React.FC = () => {
         <h1 className={styles.homePage__title}>
           Welcome to Nice Gargets store!
         </h1>
+        <BannerSlider />
         <CardCarousel title='Brand new models' endpoint='new'/>
         <Categories />
         <CardCarousel title='Hot prices' endpoint='discount'/>
