93c9a852197e302ef2095149ce5f0d19a19c415e
Yuliia Kucheriavenko <kucherisvenko@gmail.com>
2023-08-10 23:04:22 +0300

    started add Contacts Page

---
+++ src/pages/ContactsPage/ContactsPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './CartPage.module.scss';
+
+export const ContactsPage = () => {
+  return (
+    <>
+      <h1 className={styles.title}>Our team</h1>
+    </>
+  );
+};
