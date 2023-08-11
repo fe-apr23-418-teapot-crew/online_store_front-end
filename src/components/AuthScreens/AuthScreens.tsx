79aa5df10ed83806f4013742b4f2711a13a5f2a4
Maksym Bobryk <booobryyyk@gmail.com>
2023-08-11 04:52:20 +0300

    fixed overflow

--- src/components/AuthScreens/AuthScreens.tsx
+++ src/components/AuthScreens/AuthScreens.tsx
@@ -131,8 +131,7 @@ export const AuthScreens: React.FC<Props> = ({
   };
   useEffect(() => {
     const originalStyle = window.getComputedStyle(document.body).overflow;
-    document.body.style.overflow = 'hidden';
-  
+
     return () => {
       document.body.style.overflow = originalStyle;
     };
